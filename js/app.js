decf19af9be985e6e428c39de729c411cddb43ae
baradweelrawan <bardaweel_rawan@yahoo.com>
2021-04-26 15:29:36 +0300

    get Q1

--- js/app.js
+++ js/app.js
@@ -2,7 +2,24 @@
 alert ('WELCOME IN About Me Website')
 let username=  prompt('Do You Want To Know More Pleas Enter ? (Yes,No)');
 console.log(username);
-let Question1=  prompt('can you Guess How Many Years I SPend Studing At University ? (Yes,No)');
-console.log(Question1);
+if (username == Yes)
+{console.log(username);
+
+}
+let Question1=  prompt('can you Guess How Many Years I SPend Studing At University ?  (Yes,No)');
+switch (Question1)
+{
+    case 'yes':
+        document.write(<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHJkCzmAxbfUw/company-logo_200_200/0/1564447718426?e=2159024400&v=beta&t=RPlkOzZdPKod2JGACqqmFvp_gg4GYdkr2f6ARaoc4Fg"
+            alt="true" 
+            />);
+            break;
+            case 'No':
+                console.log('You can\'t guess');
+            break;
+
+
+}
+
 
 
