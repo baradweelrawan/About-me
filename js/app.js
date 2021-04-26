2281a8ec05fec4cf13a42a49f8a040ab54a60ea4
baradweelrawan <bardaweel_rawan@yahoo.com>
2021-04-26 15:09:18 +0300

    get question1

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@
 alert ('WELCOME IN About Me Website')
 let username=  prompt('Do You Want To Know More Pleas Enter ? (Yes,No)');
 console.log(username);
+let Question1=  prompt('can you Guess How Many Years I SPend Studing At University ? (Yes,No)');
+console.log(Question1);
+
+
