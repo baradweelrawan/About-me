cd0ac93d39d5a3438ee4420a8721e7628f072923
baradweelrawan <bardaweel_rawan@yahoo.com>
2021-04-26 15:41:26 +0300

    get question2

--- js/app.js
+++ js/app.js
@@ -6,18 +6,28 @@ alert ('WELCOME IN About Me Website')
 //{console.log(username);
 
 //}
-let Question1=  prompt('can you Guess How Many Years I SPend Studing At University ?  (Yes,No)');
+let Question1=  prompt('can you Guess How Many Years I SPend Studing At University ?  (Yes,No)').toLowerCase;
 switch (Question1)
 {
     case 'yes':
-        document.write(<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHJkCzmAxbfUw/company-logo_200_200/0/1564447718426?e=2159024400&v=beta&t=RPlkOzZdPKod2JGACqqmFvp_gg4GYdkr2f6ARaoc4Fg"
-            alt="true" 
-            />);
+        console.log('You can guess');
             break;
-            case 'No':
-                console.log('You can\'t guess');
+    case 'No':
+        console.log('You can\'t guess');
             break;
 }
+let Question2=  prompt('can you Guess if i have a problem of going to my university').toLowerCase;
+switch (Question2)
+{
+    case 'yes':
+        console.log('You can guess');
+            break;
+    case 'No':
+        console.log('You can\'t guess');
+            break;
+}
+
+
 
 
 
